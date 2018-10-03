1cf2ca0331b947f5131bc0bfb207f8c9b7842127
Unknown <mckim8686@gmail.com>
2018-10-03 10:07:30 +0900

    기간 추가중/period추가
    기간쿼리 버그인듯?

--- lemsquery/views.py
+++ lemsquery/views.py
@@ -1,4 +1,4 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 from django.conf import settings
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -42,7 +42,8 @@ class DeviceView(ListView):
         return  sorted(devices, key=lambda device: device['serial_id'])
 
 class DownloadView(View):
-    fields = [
+    fields_for_no = [
+        'LogicalId',
         'PhaseVoltage',
         'LineVoltage',
         'VoltageCrestFactor',
@@ -66,34 +67,168 @@ class DownloadView(View):
         'AccumulatePowerConsumption',
     ]
 
-    def dt_to_ns(dt):        
+    fields_for_period = [
+        'LogicalId',
+        'PhaseVoltageMax',
+        'PhaseVoltageAvg',
+        'PhaseVoltageFirst',
+        'PhaseVoltageLast',
+        'LineVoltageMin',
+        'LineVoltageMax',
+        'LineVoltageAvg',
+        'LineVoltageFirst',
+        'LineVoltageLast',
+        'VoltageCrestFactorMin',
+        'VoltageCrestFactorMax',
+        'VoltageCrestFactorAvg',
+        'VoltageCrestFactorFirst',
+        'VoltageCrestFactorLast',
+        'PhaseCurrentMin',
+        'PhaseCurrentMax',
+        'PhaseCurrentAvg',
+        'PhaseCurrentFirst',
+        'PhaseCurrentLast',
+        'CurrentCrestFactorMin',
+        'CurrentCrestFactorMax',
+        'CurrentCrestFactorAvg',
+        'CurrentCrestFactorFirst',
+        'CurrentCrestFactorLast',
+        'ActivePowerMin',
+        'ActivePowerMax',
+        'ActivePowerAvg',
+        'ActivePowerFirst',
+        'ActivePowerLast',
+        'ReactivePowerMin',
+        'ReactivePowerMax',
+        'ReactivePowerAvg',
+        'ReactivePowerFirst',
+        'ReactivePowerLast',
+        'PowerFactorMin',
+        'PowerFactorMax',
+        'PowerFactorAvg',
+        'PowerFactorFirst',
+        'PowerFactorLast',
+        'FrequencyMin',
+        'FrequencyMax',
+        'FrequencyAvg',
+        'FrequencyLast',
+        'ThdPhaseVoltageMin',
+        'ThdPhaseVoltageMax',
+        'ThdPhaseVoltageAvg',
+        'ThdPhaseVoltageFirst',
+        'ThdPhaseVoltageLast',
+        'ThdPhaseCurrentMin',
+        'ThdPhaseCurrentMax',
+        'ThdPhaseCurrentAvg',
+        'ThdPhaseCurrentFirst',
+        'ThdPhaseCurrentLast',
+        'TemperatureMin',
+        'TemperatureMax',
+        'TemperatureAvg',
+        'TemperatureFirst',
+        'TemperatureLast',
+        'VoltageHarmonicsMin',
+        'VoltageHarmonicsMax',
+        'VoltageHarmonicsAvg',
+        'VoltageHarmonicsFirst',
+        'VoltageHarmonicsLast',
+        'CurrentHarmonicsMin',
+        'CurrentHarmonicsMax',
+        'CurrentHarmonicsAvg',
+        'CurrentHarmonicsFirst',
+        'CurrentHarmonicsLast',
+        'ApparentPowerMin',
+        'ApparentPowerMax',
+        'ApparentPowerAvg',
+        'ApparentPowerFirst',
+        'ApparentPowerLast',
+        'InverterConversionEfficiencyMin',
+        'InverterConversionEfficiencyMax',
+        'InverterConversionEfficiencyAvg',
+        'InverterConversionEfficiencyFirst',
+        'InverterConversionEfficiencyLast',
+        'ElectricChargeMin',
+        'ElectricChargeMax',
+        'ElectricChargeAvg',
+        'ElectricChargeFirst',
+        'ElectricChargeLast',
+        'LineVoltageRSMin',
+        'LineVoltageRSMax',
+        'LineVoltageRSAvg',
+        'LineVoltageRSFirst',
+        'LineVoltageRSLast',
+        'LineVoltageSTMin',
+        'LineVoltageSTMax',
+        'LineVoltageSTAvg',
+        'LineVoltageSTFirst',
+        'LineVoltageSTLast',
+        'LineVoltageTRMin',
+        'LineVoltageTRMax',
+        'LineVoltageTRAvg',
+        'LineVoltageTRFirst',
+        'LineVoltageTRLast',
+        'AccumulatePowerConsumptionMin',
+        'AccumulatePowerConsumptionMax',
+        'AccumulatePowerConsumptionAvg',
+        'AccumulatePowerConsumptionFirst',
+        'AccumulatePowerConsumptionLast',
+        'AccumulatePowerConsumptionIncrement',
+        'RatePerKWH',
+        'TimeStep',
+    ]
+
+    periods_dic = {
+        'no': '',
+        '1m': '-1-min',
+        '5m': '-5-min',
+        '15m': '-15-min',
+        '1h': '-1-hour',
+        '1d': '-1-day',
+        '1w': '-1-week',
+        '1mo': '-1-month',
+        '1y': '-1-year',
+    }
+
+    def dt_to_ns(self, date_string,offset=0):        
         epoch = datetime.utcfromtimestamp(0)
-        dt = datetime.strptime(dt, '%m/%d/%Y')
-        delta = dt - epoch
-        return int(delta.total_seconds() * 1000 * 1000)
+        dt = timedelta(seconds=offset)
+        t0 = datetime.strptime(date_string, '%m/%d/%Y')
+        delta = (dt + t0) - epoch
+        return int(delta.total_seconds() * 1000 * 1000 * 1000)
 
     def get(self, request, site_id, logical_id):
         # Create the HttpResponse object with the appropriate CSV header.
         response = HttpResponse(content_type='text/csv')
-        response['Content-Disposition'] = 'attachment; filename="%d_%s_%d.csv"'%(site_id, logical_id,int(time.time()))
+        response['Content-Disposition'] = "attachment; filename='%d_%s_%d.csv'"%(site_id, logical_id,int(time.time()))
 
-        url = 'https://www.energywatch.kr/api/v2/metering-ac/group'
+        period = request.GET['period']
+
+        url = 'https://www.energywatch.kr/api/v2/metering-ac/group' + self.periods_dic[period]
         params = copy.deepcopy(payload)
         params['site_id'] = site_id
         params['metering_device_logical_id'] = logical_id
         
-        
-        start = self.dt_to_ns(request.GET['start'])
-        end = self.dt_to_ns(request.GET['end'])
-        print(start, end)
+        start_string = request.GET['start']
+        end_string = request.GET['end']
+        if(start_string):
+            params['start_ts'] = self.dt_to_ns(start_string)
+        if(end_string):
+            params['end_ts'] = self.dt_to_ns(end_string,86399)
 
-        r = requests.get(url, params=payload, headers=headers)
+        r = requests.get(url, params=params, headers=headers)
+        print(r.url)
         resp = r.json()['_data_']
         writer = csv.writer(response)
 
+        fields = []
+        if(period == 'no'):
+            fields = self.fields_for_no
+        else:
+            fields = self.fields_for_period
+
         meterings = list(filter(lambda m: m['GroupId'] == 1, resp[logical_id]))
-        meterings = list(map(lambda m: [base64.b64decode(m['CreatedDate']).decode('utf-8')]+[m[d] for d in m if d in self.fields], meterings))
-        writer.writerow(['CreatedDate'] + self.fields)
+        meterings = list(map(lambda m: [base64.b64decode(m['CreatedDate']).decode('utf-8')]+[m[d] for d in m if d in fields], meterings))
+        writer.writerow(['CreatedDate'] + fields)
         writer.writerows(meterings)
 
         return response
